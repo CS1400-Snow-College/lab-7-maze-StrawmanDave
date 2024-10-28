3c8ebc8931613a4df8d213cadfb46bd0c06bae9e
StrawmanDave <davidgb.strawman@gmail.com>
2024-10-28 14:49:15 -0700

    Printing the map out

--- Program.cs
+++ Program.cs
@@ -4,3 +4,10 @@ Console.Clear();
 
 Console.Write("This is a maze that getting to the '*' will complete the maze");
 Console.WriteLine();
+
+string[] mapRows = File.ReadAllLines("map.txt");
+
+for(int i = 0; i<mapRows.Length; i++)
+{
+    Console.WriteLine(mapRows[i]);
+}
